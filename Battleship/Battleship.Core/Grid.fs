9ecbf889fdeccf78df3fb57b3cfc560c981d147a
Benjijamin <benjicourriel@gmail.com>
2025-06-02 19:38:27 -0400

    elementAt

--- Battleship/Battleship.Core/Grid.fs
+++ Battleship/Battleship.Core/Grid.fs
@@ -10,3 +10,17 @@ module Grid =
 
     (* ------- À COMPLÉTER ------- *)
     (* --- Nouvelles fonctions --- *)
+
+    let elementAt (g : 'a Grid) (x : int) (y : int) : 'a option =
+        let rec loopColumn g y = 
+            match g with
+            | Empty -> None
+            | Row(l, n) when y = 0 -> 
+                let rec loopRow l i =
+                    match l with
+                    | [] -> None
+                    | h::t when i = 0 -> Some h
+                    | h::t -> loopRow t (i-1)
+                loopRow l x
+            | Row(l, n) -> loopColumn n (y-1)
+        loopColumn g y
