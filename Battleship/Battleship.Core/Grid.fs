74fa650ed1aae77008be5663d2b9a1ca740e3669
Benjijamin <benjicourriel@gmail.com>
2025-06-15 14:27:10 -0400

    parcours avec index

--- Battleship/Battleship.Core/Grid.fs
+++ Battleship/Battleship.Core/Grid.fs
@@ -32,18 +32,33 @@ module Grid =
             let largeur = List.length ligne
             (hauteur, largeur)
 
-    let parcoursGrid (lambda : 'a -> 'b -> 'b) (acc: 'b) (grid: 'a Grid) : 'b =
-        let rec loopColumn acc grid =
+
+    let parcoursGridAvecIndex (lambda : 'a -> 'b -> Coord -> 'b) (acc: 'b) (grid: 'a Grid) : 'b =
+        let rec loopColumn acc columnIndex grid =
             match grid with
             | Empty -> acc
             | Row(ligne, reste) ->
-                let rec loopRow acc liste =
+                let rec loopRow acc rowIndex liste =
                     match liste with
                     | [] -> acc
-                    | h::t -> loopRow (lambda h acc) t
-                loopColumn (loopRow acc ligne) reste
+                    | h::t -> loopRow (lambda h acc (columnIndex, rowIndex)) (rowIndex + 1) t
+                loopColumn (loopRow acc 0 ligne) (columnIndex + 1) reste
 
-        loopColumn acc grid
+        loopColumn acc 0 grid
+        
+    let parcoursGrid (lambda : 'a -> 'b -> 'b) (acc: 'b) (grid: 'a Grid) : 'b =
+        parcoursGridAvecIndex (fun a b c -> lambda a b) acc grid
+
+        //let rec loopX r l x y = 
+        //    match r with
+        //    | [] -> l
+        //    | Active(name, i)::t -> loopX t (l@[((y, x), Active(name, i))]) (x+1) y
+        //    | _::t -> loopX t l (x+1) y
+
+        //let rec loopY g l y =
+        //    match g with
+        //    | Empty -> l
+        //    | Row(r, n) -> loopY n (l@(loopX r [] 0 y)) (y+1)
 
     // utiliser dans la fonction addShip. Juliette
     let makeCoordIndexMap (coords: 'a list)(f: int -> 'a -> 'b * int) =
